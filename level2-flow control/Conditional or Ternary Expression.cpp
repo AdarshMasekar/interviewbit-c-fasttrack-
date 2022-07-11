1d21bade471eba422128d733d9bf79df79fd5915
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-07-11 23:02:37 +0530

    Create Conditional or Ternary Expression.cpp

---
+++ level2-flow control/Conditional or Ternary Expression.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()  {
+    int x, y;
+  
+    cin>>x>>y;
+    // YOUR CODE GOES HERE
+    string b= (x <= y) ? "Robin":"Rahul";
+    cout<<b<<endl;
+    return 0;
+}
