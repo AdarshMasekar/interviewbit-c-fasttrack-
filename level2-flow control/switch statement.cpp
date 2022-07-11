0cad12c63f60e7d051a53493c5c8718e8678b6b6
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-07-11 23:01:51 +0530

    Create switch statement.cpp

---
+++ level2-flow control/switch statement.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int weekday;
+    cin>>weekday;
+    // YOUR CODE GOES HERE
+    switch(weekday)
+    {
+        case 1:
+        cout<<"Monday"<<endl;
+        case 2:
+        cout<<"Tuesday"<<endl;
+        case 3:
+        cout<<"Wednesday"<<endl;
+        case 4:
+        cout<<"Thursday"<<endl;
+        case 5:
+        cout<<"Friday"<<endl;
+        case 6:
+        cout<<"Saturday"<<endl;
+        case 7:
+        cout<<"Sunday"<<endl;
+    }
+    return 0;
+}
