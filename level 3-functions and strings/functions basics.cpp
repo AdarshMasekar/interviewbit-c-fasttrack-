df38bfb6bd54ce39de3a8d03f33ba3ea2d15629b
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-07-12 21:15:09 +0530

    Create functions basics.cpp

---
+++ level 3-functions and strings/functions basics.cpp
@@ -0,0 +1,28 @@
+
+/*
+#include<iostream>
+using namespace std;
+#include<math.h>
+*/
+
+// Your Code goes here
+
+int compute(int A,int B){
+    int sum;
+    sum=(A*A)+(B*B);
+    return sum;
+}
+
+
+
+
+
+
+/*
+int main()  {
+    int A, B;
+    cin>>A>>B;
+    cout<<compute(A, B)<<endl;
+    return 0;
+}
+*/
