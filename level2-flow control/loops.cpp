b27cef79d1974c8cf305572dbe59a5d0a1ba3c0b
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-07-12 20:40:41 +0530

    Update loops.cpp

--- level2-flow control/loops.cpp
+++ level2-flow control/loops.cpp
@@ -1,3 +1,23 @@
+
+for loop
+// #include<iostream>
+
+// using namespace std;
+
+// int main()  {
+//     int N;
+//     cin>>N;
+//     // YOUR CODE GOES HERE
+//     for(int i=0;i<N;i++){
+//         if(i%2!=0){
+//             cout<<i<<endl;
+//         }
+//     }    
+//     return 0;
+// }
+
+while loop 
+
 #include<iostream>
 
 using namespace std;
@@ -5,11 +25,13 @@ using namespace std;
 int main()  {
     int N;
     cin>>N;
+    int i=0;
     // YOUR CODE GOES HERE
-    for(int i=0;i<N;i++){
+    while(i<N){
         if(i%2!=0){
             cout<<i<<endl;
         }
+        i++;
     }    
     return 0;
 }
